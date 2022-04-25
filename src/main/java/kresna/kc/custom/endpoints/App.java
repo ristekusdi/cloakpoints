46fbf05e9febf8d4c94f3b97347d45428fe81f9a
Satya Kresna <satyakresna@unud.ac.id>
2022-04-25 12:32:09 +0800

    initial commit

---
+++ src/main/java/kresna/kc/custom/endpoints/App.java
@@ -0,0 +1,13 @@
+package kresna.kc.custom.endpoints;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
