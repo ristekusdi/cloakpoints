656e4005e2f354eb1a44394d96c6053404a21c78
Kresna Satya <satyakresna6295@gmail.com>
2022-04-26 17:37:30 +0800

    expirement with Keycloak methods

--- src/main/java/kresna/kc/custom/endpoints/KcResourceProvider.java
+++ src/main/java/kresna/kc/custom/endpoints/KcResourceProvider.java
@@ -3,19 +3,33 @@ package kresna.kc.custom.endpoints;
 import lombok.RequiredArgsConstructor;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.UserModel;
+import org.keycloak.models.utils.ModelToRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
 import org.keycloak.services.managers.AppAuthManager;
 import org.keycloak.services.managers.AuthenticationManager.AuthResult;
 import org.keycloak.services.resource.RealmResourceProvider;
+import org.keycloak.utils.SearchQueryUtils;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @RequiredArgsConstructor
 public class KcResourceProvider implements RealmResourceProvider {
 
     private final KeycloakSession session;
+    private final AuthResult auth;
+
+    public KcResourceProvider(KeycloakSession session) {
+        this.session = session;
+        this.auth = new AppAuthManager.BearerTokenAuthenticator(session).authenticate();
+    }
 
     @Override
     public Object getResource() {
@@ -30,18 +44,37 @@ public class KcResourceProvider implements RealmResourceProvider {
     @GET
     @Path("users")
     @Produces(MediaType.APPLICATION_JSON)
-    public Stream<UserModel> users() {
-        return session.users().getUsersStream(session.getContext().getRealm());
-    }
-
-    private AuthResult checkAuth() {
-        AuthResult auth = new AppAuthManager.BearerTokenAuthenticator(session).authenticate();
-        if (auth == null) {
+    public List<UserRepresentation> users(@QueryParam("search") String search,
+                                          @QueryParam("username") String username,
+                                          @QueryParam("q") String searchQuery) {
+        if (this.auth == null || this.auth.getToken() == null) {
             throw new NotAuthorizedException("Bearer");
-        } else if (auth.getToken().getIssuedFor() == null) {
-            throw new ForbiddenException("Token is not properly issued");
         }
 
-        return auth;
+        Map<String, String> attributes = new HashMap<>();
+
+        Map<String, String> searchAttributes = searchQuery == null ? Collections.emptyMap() : SearchQueryUtils.getFields(searchQuery);
+
+        if (search != null || username != null) {
+            if (search != null) {
+                attributes.put(UserModel.SEARCH, search);
+            }
+
+            if (username != null) {
+                attributes.put(UserModel.USERNAME, username);
+            }
+
+//            attributes.putAll(searchAttributes);
+        }
+
+        Stream<UserModel> userModels = session.users().searchForUserStream(session.getContext().getRealm(), attributes);
+        List<UserRepresentation> users =  userModels.map(userModel -> ModelToRepresentation.toRepresentation(session, session.getContext().getRealm(), userModel))
+                .collect(Collectors.toList());
+
+        return users;
+//        return session.users().getUsersStream(session.getContext().getRealm())
+//                .map(userModel -> ModelToRepresentation.toRepresentation(session, session.getContext().getRealm(), userModel))
+//                .collect(Collectors.toList());
     }
+
 }
