db6ae0f536c00ef4d9796e89dcd99968fed5e670
Satya Kresna <satyakresna@unud.ac.id>
2022-04-27 16:44:24 +0800

    successfuly implement search users with search and searchQuery attribute

--- src/main/java/kresna/kc/custom/endpoints/KcResourceProvider.java
+++ src/main/java/kresna/kc/custom/endpoints/KcResourceProvider.java
@@ -10,13 +10,11 @@ import org.keycloak.services.managers.AuthenticationManager.AuthResult;
 import org.keycloak.services.resource.RealmResourceProvider;
 import org.keycloak.utils.SearchQueryUtils;
 
+import javax.management.Query;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -45,36 +43,59 @@ public class KcResourceProvider implements RealmResourceProvider {
     @Path("users")
     @Produces(MediaType.APPLICATION_JSON)
     public List<UserRepresentation> users(@QueryParam("search") String search,
-                                          @QueryParam("username") String username,
-                                          @QueryParam("q") String searchQuery) {
+                                          @QueryParam("q") String searchQuery,
+                                          @QueryParam("first") Integer firstResult,
+                                          @QueryParam("max") Integer maxResults) {
         if (this.auth == null || this.auth.getToken() == null) {
             throw new NotAuthorizedException("Bearer");
         }
 
         Map<String, String> attributes = new HashMap<>();
 
-        Map<String, String> searchAttributes = searchQuery == null ? Collections.emptyMap() : SearchQueryUtils.getFields(searchQuery);
+        Map<String, List<String>> searchAttributes = setAttributes(searchQuery);
 
-        if (search != null || username != null) {
-            if (search != null) {
-                attributes.put(UserModel.SEARCH, search);
-            }
-
-            if (username != null) {
-                attributes.put(UserModel.USERNAME, username);
-            }
-
-//            attributes.putAll(searchAttributes);
+        if (search != null) {
+            attributes.put(UserModel.SEARCH, search);
         }
 
         Stream<UserModel> userModels = session.users().searchForUserStream(session.getContext().getRealm(), attributes);
         List<UserRepresentation> users =  userModels.map(userModel -> ModelToRepresentation.toRepresentation(session, session.getContext().getRealm(), userModel))
                 .collect(Collectors.toList());
 
-        return users;
-//        return session.users().getUsersStream(session.getContext().getRealm())
-//                .map(userModel -> ModelToRepresentation.toRepresentation(session, session.getContext().getRealm(), userModel))
-//                .collect(Collectors.toList());
+        List<UserRepresentation> selectedUsers = new ArrayList<>();
+
+        for (Map.Entry<String, List<String>> entry : searchAttributes.entrySet()) {
+            for (int i = 0; i < users.size(); i++) {
+                if (users.get(i).getAttributes().containsKey(entry.getKey())) {
+                    if (users.get(i).getAttributes().containsValue(entry.getValue())) {
+                        selectedUsers.add(users.get(i));
+                    }
+                }
+            }
+        }
+
+        if (firstResult == null || firstResult <= 0) {
+            firstResult = 0;
+        }
+
+        if (maxResults == null || maxResults <= 0) {
+            maxResults = 20;
+        }
+
+        selectedUsers = selectedUsers.stream().distinct().skip(firstResult).limit(maxResults).collect(Collectors.toList());
+        return selectedUsers;
     }
 
+
+    private Map<String, List<String>> setAttributes(String q) {
+        String[] splitKv = q.split(" ");
+        Map<String, List<String>> attributes = new HashMap<>();
+        for (String item: splitKv) {
+            String[] kv = item.split(":");
+
+            List<String> vList = new ArrayList<>(Arrays.asList(kv[1].trim().split(",")));
+            attributes.put(kv[0].trim(), vList);
+        }
+        return attributes;
+    }
 }
